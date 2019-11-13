2d7e26aba2300b8453d7e7c303f252fc0b021adb
Thane Engen <thaneengen@gmail.com>
2019-11-13 14:05:28 -0600

    Refactor 'navbar' into separate React component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,43 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
 import HomeParticles from "./HomeParticles";
-import logo from "../imgs/thane-engen-logo.png";
 
 const HomePage = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-sm navbar-dark">
-        <Link to="#" className="navbar-brand">
-          <img src={logo} alt="Thane Engen Logo" />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link className="nav-item nav-link" to="#">
-              About
-            </Link>
-            <Link className="nav-item nav-link" to="#">
-              Skills
-            </Link>
-            <Link className="nav-item nav-link" to="#">
-              Projects
-            </Link>
-            <Link className="nav-item nav-link" to="#">
-              Contact
-            </Link>
-          </div>
-        </div>
-      </nav>
+      <NavBar />
       <HomeParticles />
       <div className="text-center">
         <h1>My Name is Thane Engen and I am a Web Developer</h1>
---
+++ src/components/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../imgs/thane-engen-logo.png";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark">
+      <Link to="#" className="navbar-brand">
+        <img src={logo} alt="Thane Engen Logo" />
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link className="nav-item nav-link" to="#">
+            About
+          </Link>
+          <Link className="nav-item nav-link" to="#">
+            Skills
+          </Link>
+          <Link className="nav-item nav-link" to="#">
+            Projects
+          </Link>
+          <Link className="nav-item nav-link" to="#">
+            Contact
+          </Link>
+          <Link className="nav-item nav-link" to="#">
+            Resume
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
