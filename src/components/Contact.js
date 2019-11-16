d14a0627c6d7750366259158afe06d9894075dca
Thane Engen <thaneengen@gmail.com>
2019-11-16 14:43:17 -0600

    Add email form functionality with emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,67 @@
 import React from "react";
 
-const Contact = () => {
-  return (
-    <div id="contact">
-      <h2>Contact</h2>
-    </div>
-  );
-};
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { feedback: "", name: "Name", email: "email@example.com" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-export default Contact;
+  render() {
+    return (
+      <div id="contact">
+        <h2>Contact</h2>
+        <form className="test-mailing">
+          <h1>Let's see if it works</h1>
+          <div>
+            <textarea
+              id="test-mailing"
+              name="test-mailing"
+              onChange={this.handleChange}
+              placeholder="Post some lorem ipsum here"
+              required
+              value={this.state.feedback}
+              style={{ width: "100%", height: "150px" }}
+            />
+          </div>
+          <input
+            type="button"
+            value="Submit"
+            className="btn btn--submit"
+            onClick={this.handleSubmit}
+          />
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(event) {
+    this.setState({ feedback: event.target.value });
+  }
+
+  handleSubmit(event) {
+    const templateId = "template_fo7tCjdD";
+
+    this.sendFeedback(templateId, {
+      message_html: this.state.feedback,
+      from_name: this.state.name,
+      reply_to: this.state.email
+    });
+  }
+
+  sendFeedback(templateId, variables) {
+    window.emailjs
+      .send("gmail", templateId, variables)
+      .then(res => {
+        console.log("Email successfully sent!");
+      })
+      // Handle errors here however you like, or use a React error boundary
+      .catch(err =>
+        console.error(
+          "Oh well, you failed. Here some thoughts on the error that occured:",
+          err
+        )
+      );
+  }
+}
