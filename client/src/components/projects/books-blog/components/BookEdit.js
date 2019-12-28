c63bf7047745b38fd875751f0194c745b8c7f6a9
Thane Engen <thaneengen@gmail.com>
2019-12-28 15:52:16 -0600

    Fix update route

--- client/src/components/projects/books-blog/components/BookEdit.js
+++ client/src/components/projects/books-blog/components/BookEdit.js
@@ -1,7 +1,8 @@
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
 import { getBook, editBook } from "../actions/";
-
+import BookForm from "./BookForm";
 import "../../../../styles/book-blog/main.scss";
 
 class BookEdit extends React.Component {
@@ -9,62 +10,17 @@ class BookEdit extends React.Component {
     this.props.getBook(this.props.match.params.id);
   }
 
-  handleSubmit = formValues => {
+  onSubmit = formValues => {
     this.props.editBook(this.props.match.params.id, formValues);
   };
 
   render() {
-    console.log(this.props);
-    const book = this.props.book;
-
     return (
       <div className="main">
-        <div className="container pt-5">
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <label>Title:</label>
-              <input
-                className="form-control form-control-lg"
-                type="text"
-                name={book.title}
-                defaultValue={book.title}
-              />
-            </div>
-            <div className="form-group">
-              <label>Author:</label>
-              <input
-                className="form-control form-control-lg"
-                type="text"
-                name={book.author}
-                defaultValue={book.author}
-              />
-            </div>
-            <div className="form-group">
-              <label>Image:</label>
-              <input
-                className="form-control form-control-lg"
-                type="text"
-                name={book.image}
-                defaultValue={book.image}
-              />
-            </div>
-            <div className="form-group">
-              <label>Description:</label>
-              <textarea
-                id="description-input"
-                className="form-control"
-                type="text"
-                name={book.desc}
-                defaultValue={book.desc}
-              />
-            </div>
-            <input
-              className="btn btn-warning btn-lg"
-              value="Update"
-              type="submit"
-            />
-          </form>
-        </div>
+        <BookForm
+          initialValues={_.pick(this.props.book, "title", "author", "image", "desc")}
+          onSubmit={this.onSubmit} 
+        />
       </div>
     );
   }
