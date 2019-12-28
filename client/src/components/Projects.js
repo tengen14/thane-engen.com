d93a41890618a133e59766ec9ac755ffa50b84e9
Thane Engen <thaneengen@gmail.com>
2019-12-28 17:29:35 -0600

    Set target='_blank' for project routes

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -5,9 +5,9 @@ const Projects = () => {
   return (
     <div id="projects">
       <h2>Projects</h2>
-      <Link to="/wikipedia-viewer">Wikipedia Article Searcher</Link>
-      <Link to="/recipe-box">Recipe Box</Link>
-      <Link to="/book-blog">Book Blog</Link>
+      <Link to="/recipe-box" target="_blank">Recipe Box</Link>
+      <Link to="/wikipedia-viewer" target="_blank">Wikipedia Article Searcher</Link>
+      <Link to="/book-blog" target="_blank">Book Blog</Link>
     </div>
   );
 };
