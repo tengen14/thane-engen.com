25d0e7bed4dc775d1cd0a23fdf1eae812bfff2b9
Thane Engen <thaneengen@gmail.com>
2019-12-30 14:00:19 -0600

    Add project images to homepage

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import recipebox from "../imgs/projects/recipe-box.png";
+import wikipediaviewer from "../imgs/projects/wikipedia-viewer.png";
+import bookblog from "../imgs/projects/book-blog.png";
+
 const Projects = () => {
   return (
     <div id="projects">
-      <h2>Projects</h2>
-      <Link to="/recipe-box" target="_blank">Recipe Box</Link>
-      <Link to="/wikipedia-viewer" target="_blank">Wikipedia Article Searcher</Link>
-      <Link to="/book-blog" target="_blank">Book Blog</Link>
+      <div className="container">
+        <h2>Projects</h2>
+        <Link to="/recipe-box" target="_blank">
+          <img src={recipebox} />
+        </Link>
+        <Link to="/wikipedia-viewer" target="_blank">
+          <img src={wikipediaviewer} />
+        </Link>
+        <Link to="/book-blog" target="_blank">
+          <img src={bookblog} />
+        </Link>
+      </div>
     </div>
   );
 };
