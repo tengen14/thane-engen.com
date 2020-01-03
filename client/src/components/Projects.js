1418da57424e7cc581e101170acb6bd0834fad1f
Thane Engen <thaneengen@gmail.com>
2020-01-03 13:42:20 -0600

    Add media responsiveness for projects

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -8,11 +8,12 @@ import bookblog from "../imgs/projects/book-blog.png";
 const Projects = () => {
   return (
     <div id="projects">
+      <h2>Projects</h2>
       <div className="container">
-        <h2>Projects</h2>
+        
         <div className="row">
-
-          <div className="col-sm">
+          {/* Recipe Box */}
+          <div className="col-md-4 col-sm-6 col-6">
             <div className="card">
               <img src={recipebox} className="card-img-top" />
               <div className="card-body">
@@ -43,7 +44,8 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className="col-sm">
+          {/* Wikipedia */}
+          <div className="col-md-4 col-sm-6 d-sm-block d-none">
             <div className="card">
               <img src={wikipediaviewer} className="card-img-top" />
               <div className="card-body">
@@ -75,7 +77,8 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className="col-sm">
+          {/* Book Blog */}
+          <div className="col-md-4 d-md-block d-none">
             <div className="card">
               <img src={bookblog} className="card-img-top" />
               <div className="card-body">
@@ -105,8 +108,77 @@ const Projects = () => {
               </div>
             </div>
           </div>
-       
         </div>
+
+        {/* Wikipedia */}
+        <div className="row">
+          <div className="col-6 d-sm-none d-block">
+            <div className="card">
+              <img src={wikipediaviewer} className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">Wikipedia Viewer</h5>
+                <p className="card-text">
+                  A basic search bar used to find articles using the Wikipedia
+                  API
+                </p>
+                <div className="d-flex justify-content-around">
+                  <Link
+                    to="/wikipedia-viewer"
+                    target="_blank"
+                    className="btn btn-success"
+                    title="See in Browser"
+                  >
+                    View Project
+                  </Link>
+                  <a
+                    href="https://github.com/tengen14/Wikipedia-Viewer"
+                    target="_blank"
+                    className="btn btn-danger"
+                    title="See Source Code and Description on Github"
+                  >
+                    <span>Learn More</span>
+                    <i className="fab fa-github-square" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Book Blog */}
+        <div className="row">
+          <div className="d-md-none col-sm-6 col-6">
+            <div className="card">
+              <img src={bookblog} className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">Book Blog</h5>
+                <p className="card-text">
+                  A pseudo book blog with CRUD functionality
+                </p>
+                <div className="d-flex justify-content-around">
+                  <Link
+                    to="/book-blog"
+                    target="_blank"
+                    className="btn btn-success"
+                    title="See in Browser"
+                  >
+                    View Project
+                  </Link>
+                  <a
+                    href="https://github.com/tengen14/Book-Blog"
+                    target="_blank"
+                    className="btn btn-danger"
+                    title="See Source Code and Description on Github"
+                  >
+                    <span>Learn More</span>
+                    <i className="fab fa-github-square" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      
       </div>
     </div>
   );
