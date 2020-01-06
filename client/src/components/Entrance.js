f26e7890d003db36fe92a1765bf90aa0c14fe1f5
Thane Engen <thaneengen@gmail.com>
2020-01-06 17:54:15 -0600

    Fix centering on enter site content

--- client/src/components/Entrance.js
+++ client/src/components/Entrance.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Entrance = () => {
   return (
-    <div id="entrance">
-      <div id="enter-site-content" className="text-center">
+    <div id="entrance" className="d-flex align-items-center">
+      <div id="enter-site-content" className="text-center ">
         <h1>My Name is Thane Engen and I am a Web Developer</h1>
         <a href="#projects">
           <button type="button" className="red-button">
