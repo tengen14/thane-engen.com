1e0f3520217bb25adc06d2d8a12da511ad9a103b
Thane Engen <thaneengen@gmail.com>
2020-01-20 12:28:06 -0600

    Seed 'fake' route

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require ('express');
 const routes = require ('./routes/index');
 
+const newsRoute = require('./routes/news');
+
 let app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use('/', routes);
+
+app.use('/news', newsRoute);
 
 app.listen(PORT, function () {
     console.log(`Listening on port ${PORT}`);
